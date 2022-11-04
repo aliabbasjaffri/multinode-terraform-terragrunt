88af29333e3761ef5d8d73e28ef1b681c05dc3df
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-11-04 23:05:26 +0100

    Updated eks outputs

--- terraform/aws_eks_cluster/outputs.tf
+++ terraform/aws_eks_cluster/outputs.tf
@@ -1,7 +1,11 @@
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
+
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "kubeconfig-certificate-authority-data" {
+output "eks_cluster_ca_cert" {
   value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
