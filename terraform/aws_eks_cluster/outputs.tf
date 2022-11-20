7e5ff0cac925f55fd96c2c53da8e703595ff9ca5
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-11-20 13:05:01 +0100

    Updated outputs

--- terraform/aws_eks_cluster/outputs.tf
+++ terraform/aws_eks_cluster/outputs.tf
@@ -9,3 +9,11 @@ output "eks_cluster_endpoint" {
 output "eks_cluster_ca_cert" {
   value = module.eks.cluster_certificate_authority_data
 }
+
+output "eks_cluster_primary_security_group_id" {
+  value = module.eks.cluster_primary_security_group_id
+}
+
+output "eks_cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
