6b91a6d6e3b10bdef42c6b8bd1587d52c31cd24c
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-11-24 08:56:42 +0100

    Deleted kubernetes manifests

--- terraform/kubernetes_manifest/main.tf
+++
@@ -1,6 +0,0 @@
-resource "kubernetes_manifest" "manifest" {
-  manifest = yamldecode(<<-EOF
-    ${var.kubernetes_manifest}
-    EOF
-  )
-}
--- terraform/kubernetes_manifest/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "kubernetes_manifest" {
-  type = any
-}
