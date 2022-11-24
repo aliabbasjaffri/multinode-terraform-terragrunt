40a7178c196e3764be59efa51708ae3721065661
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-11-24 08:56:21 +0100

    Optional variables

--- terraform/helm_chart/variables.tf
+++ terraform/helm_chart/variables.tf
@@ -1,16 +1,16 @@
 variable "helm_chart" {
   type = object({
     name          = string
-    namespace     = string
+    namespace     = optional(string)
     create_namespace = bool
     repository    = string
     chart         = string
     chart_version = string
     values        = any
-    set = list(object({
+    set = optional(list(object({
       name  = string
       value = any
       type  = string
-    }))
+    })))
   })
 }
