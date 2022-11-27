bea04369a065341e07ced40a7883c4b87f3a62c3
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-11-27 17:43:15 +0100

    Updated outputs from AWS EKS cluster

--- terraform/aws_eks_cluster/main.tf
+++ terraform/aws_eks_cluster/main.tf
@@ -155,9 +155,9 @@ module "vpc_cni_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 4.12"
 
-  role_name_prefix      = "VPC-CNI-IRSA"
-  # vpc_cni_enable_ipv6   = true
+  role_name_prefix = "VPC-CNI-IRSA"
   attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
 
   oidc_providers = {
     main = {
--- terraform/aws_eks_cluster/outputs.tf
+++ terraform/aws_eks_cluster/outputs.tf
@@ -2,6 +2,10 @@ output "eks_cluster_name" {
   value = var.aws_eks_cluster.name
 }
 
+output "eks_cluster_id" {
+  value = module.eks.cluster_id
+}
+
 output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
@@ -16,4 +20,8 @@ output "eks_cluster_primary_security_group_id" {
 
 output "eks_cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
+}
+
+output "eks_oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
 }
